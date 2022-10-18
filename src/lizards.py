d24472dbaa3d3638f5f0067abddcfae575e4aecb
aziz-mu <aziz20019@gmail.com>
2022-10-17 22:14:08 -0400

    initial dev commit

---
+++ src/lizards.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+# First, get a lizard tesselation to actually show up on screen. 
+# Use Processing, and get the exact line lengths for each side of the lizard outline. 
+# Then, figuring out exactly where (and how rotated) each lizard is, tesselate it onto the screen. 
+
+
