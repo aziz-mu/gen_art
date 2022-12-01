7ceacd964b2e54608e595df6329cff6b165b98e2
aziz-mu <aziz20019@gmail.com>
2022-12-01 00:21:48 -0500

    added gear video

--- src/gears.py
+++ src/gears.py
@@ -5,8 +5,11 @@ Description
 
 """
 from math import pi, sin, cos
+from cairosvg import svg2png
 import random
 import cairo
+import os
+import shutil
 
 NUMBER_OF_PIXELS = 127
 
@@ -57,6 +60,8 @@ class Gear():
             new_spoke.rotate(2*pi/num_spokes*i)
             self.spokes.append(new_spoke)
 
+    def rotate(self, angle):
+        [spoke.rotate(angle) for spoke in self.spokes] 
 
     def draw(self, context):
         [spoke.draw(context) for spoke in self.spokes]
@@ -68,18 +73,19 @@ def get_closest_dot_color(gearlist, x_coord, y_coord):
 
     min_distance = -1
     dot_color = (0,0,0)
-
+         
     for gear in gearlist:
         for spoke in gear.spokes:
             for dot in spoke.dots:
                 dist = squared_distance(x_coord, y_coord, dot.x, dot.y)
                 if (min_distance == -1 or dist < min_distance):
-                        min_distance = dist
-                        dot_color = dot.fill_color
+                    min_distance = dist
+                    dot_color = dot.fill_color
     return dot_color
 
 def draw_pixel(context, i, j):
     context.set_source_rgb(*get_closest_dot_color([gear1, gear2], i/NUMBER_OF_PIXELS*10, j/NUMBER_OF_PIXELS*10))
+    
     context.move_to(i/NUMBER_OF_PIXELS*10-0.01, j/NUMBER_OF_PIXELS*10-0.01)
     context.line_to((i+1)/NUMBER_OF_PIXELS*10+0.01, j/NUMBER_OF_PIXELS*10-0.01)
     context.line_to((i+1)/NUMBER_OF_PIXELS*10+0.01, (j+1)/NUMBER_OF_PIXELS*10+0.01)
@@ -89,16 +95,31 @@ def draw_pixel(context, i, j):
     context.stroke()
 
 
+os.mkdir("temp")
 
 gear1 = Gear(4,30,10,8,5)
 gear2 = Gear(3,20,15,2,6)
 
-with cairo.SVGSurface("gear.svg", 1000, 1000) as surface:
+FRAMES = 70
+for i in range(FRAMES):
+    gear1.rotate(2*pi/FRAMES)
+    gear2.rotate(-2*pi/FRAMES)
+    file_name = "temp/gears" + str(i).zfill(3) + ".png"
+
+    surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, 1000, 1000) 
     context = cairo.Context(surface)
-    context.scale(100,100) 
+    context.scale(100,100)
     context.set_line_width(0.03)
     context.set_source_rgb(0,0,0)
-    
-    for i in range(NUMBER_OF_PIXELS):
+    for k in range(NUMBER_OF_PIXELS):
         for j in range(NUMBER_OF_PIXELS):
-            draw_pixel(context, i, j)
+            draw_pixel(context, k, j)
+
+    # save file to png
+    surface.write_to_png(file_name)
+    print("\rFinished Frame " + str(i + 1) + " of " + str(FRAMES), end="")
+    
+
+print("Creating gif")
+os.system('ffmpeg -f image2 -hide_banner -loglevel error -start_number 0 -framerate 24 -y -i temp/gears%03d.png -loop -0 "gears.gif"')
+shutil.rmtree("temp")
